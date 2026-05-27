04e69f45f35d5ba9dff22e56476774101ed5b4d9
joshuasoliotopoulos <101246599+joshuasoliotopoulos@users.noreply.github.com>
2026-05-27 16:42:37 +0200

    Update traitement_de_donnee.py

--- traitement_de_donnee.py
+++ traitement_de_donnee.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Prétraitement des données financières
+Created on Tue Mar 31 21:40:17 2026
+
+@author: grabe
 """
 
 import os
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_PATH = os.path.join(BASE_DIR, "data_base")
-START_DATE = "2021-01-01"
+BASE_PATH = "data_base"
+
+
+
+START_DATE = "2020-10-22"
 
 all_dates = pd.date_range(
     start=START_DATE,
@@ -17,148 +22,197 @@ all_dates = pd.date_range(
     freq="D"
 )
 
-def clean_indice_dataframe(df):
 
-    df.columns = df.columns.str.strip()
+print("📂 Loading all data...\n")
 
-    if "Date" not in df.columns:
-        raise ValueError("Date manquante")
 
-    df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
-    df = df.dropna(subset=["Date"]).set_index("Date")
+# ─── DATAFRAME ────────────────────────────────────────────────────────────────────  
 
+def finalize_dataframe(df, min_period=71):
+    df = df.iloc[min_period:]
+    df = df.reset_index().rename(columns={"index": "Date"})
+    return df
+
+def prepare_dates(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.set_index("Date")
     df = df.reindex(all_dates)
-
     df["is_trading_day"] = (~df["Close"].isna()).astype(int)
+    return df
 
+def fill_data(df):
     price_cols = ["Open", "High", "Low", "Close"]
-
+    
     indicator_cols = [
         "MA20", "MA50", "Momentum", "RSI",
         "MACD", "MACD_Signal", "MACD_Hist",
-        "Volatility_20d", "BB_Upper", "BB_Lower",
-        "BB_Mid", "Return_1d", "Return_5d", "Normalized"
+        "Volatility_20d",
+        "BB_Upper", "BB_Lower", "BB_Mid",
+        "Return_1d","Return_5d",
+        "Normalized","VIX_Close"
     ]
 
-    for col in price_cols:
-        if col in df.columns:
-            df[col] = df[col].ffill()
+    df[price_cols] = df[price_cols].ffill()
+    df["Volume"] = df["Volume"].fillna(0)
 
-    if "Volume" in df.columns:
-        df["Volume"] = df["Volume"].fillna(0)
-
-    existing = [c for c in indicator_cols if c in df.columns]
-    df[existing] = df[existing].ffill()
-
-    df = df.iloc[54:].reset_index().rename(columns={"index": "Date"})
-    df[existing] = df[existing].bfill()
+    existing = [col for col in indicator_cols if col in df.columns]
+    df[existing] = df[existing].ffill().bfill()
 
     return df
 
-
-def clean_action_dataframe(df, market, sector, ticker):
-
-    df.columns = df.columns.str.strip()
-
-    if "Date" not in df.columns:
-        raise ValueError(f"Date manquante {ticker}")
-
-    df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
-    df = df.dropna(subset=["Date"]).set_index("Date")
-
-    df = df.reindex(all_dates)
-
-    df["is_trading_day"] = (~df["Close"].isna()).astype(int)
-
-    df["Market"] = market
-    df["Sector"] = sector
-
-    # 🔥 IMPORTANT FIX ICI
-    df["Ticker"] = str(ticker).strip().upper()
-
-    price_cols = ["Open", "High", "Low", "Close"]
-    volume_cols = [c for c in df.columns if "Volume" in c]
-
-    indicator_cols = [
-        "MA20", "MA50", "Momentum", "RSI",
-        "MACD", "MACD_Signal", "MACD_Hist",
-        "Volatility_20d", "BB_Upper", "BB_Lower",
-        "BB_Mid", "VIX_Close"
-    ]
-
-    for col in price_cols:
-        if col in df.columns:
-            df[col] = df[col].ffill()
-
-    df[volume_cols] = df[volume_cols].fillna(0)
-
-    existing = [c for c in indicator_cols if c in df.columns]
-    df[existing] = df[existing].ffill()
-
-    df = df.iloc[4:].reset_index().rename(columns={"index": "Date"})
-    df[existing] = df[existing].bfill()
-
+def clean_dataframe(df):
+    df = prepare_dates(df)
+    df = fill_data(df)
+    df = finalize_dataframe(df)
     return df
 
+# ─── loadding ────────────────────────────────────────────────────────────────────
+def load_indices(base_path):
+    liste=[]
+    for file in os.listdir(base_path):
+        path = os.path.join(base_path, file)
+        
+        if not file.endswith(".csv"):
+            continue
+        
+        df = pd.read_csv(path)
+        
+        df=clean_dataframe(df)
 
-def load_indices(path):
-
-    data = []
-
-    for file in os.listdir(path):
-
-        if file.endswith(".csv"):
-
-            df = pd.read_csv(os.path.join(path, file), sep=None, engine="python")
-            df = clean_indice_dataframe(df)
-
-            df["indice"] = file.replace(".csv", "")
-
-            data.append(df)
-
-    return data
-
+        df["indice"] = file.replace(".csv", "")
+        liste.append(df)
+    return liste
 
 def load_actions(base_path):
-
-    data = []
+    liste=[]
 
     for market in os.listdir(base_path):
-
         market_path = os.path.join(base_path, market)
+
         if not os.path.isdir(market_path):
             continue
 
         for sector in os.listdir(market_path):
-
             sector_path = os.path.join(market_path, sector)
-            if not os.path.isdir(sector_path):
+
+            if not os.path.isdir(sector_path) or sector.lower() == "unknown":
                 continue
 
             for file in os.listdir(sector_path):
 
-                if file.endswith(".csv"):
+                if not file.endswith(".csv") :
+                    continue
 
-                    try:
-                        ticker = file.replace(".csv", "").upper()
+                file_path = os.path.join(sector_path, file)
 
-                        df = pd.read_csv(
-                            os.path.join(sector_path, file),
-                            sep=None,
-                            engine="python"
-                        )
+                try:
+                    df = pd.read_csv(file_path)
+                    
+                    df = clean_dataframe(df)
+                    df["Ticker"] = file.replace(".csv", "")
+                    df["Market"] = market
+                    df["Sector"] = sector
+                    cols_to_check = ["Open", "High"]
+                    if df[cols_to_check].isna().any().any(): 
+                        continue 
+                    liste.append(df)
 
-                        df = clean_action_dataframe(df, market, sector, ticker)
+                except Exception as e:
+                    print(f"❌ Error {file}: {e}")
 
-                        data.append(df)
+    return liste
+# ─── MAIN ────────────────────────────────────────────────────────────────────
+def dataframe_info(df):
+    print(df.head())
+    print(df.info())
+    print(df.select_dtypes(include=['object', 'string', 'int', 'float', 'bool']).isnull().sum())
 
-                    except Exception as e:
-                        print(f"Error {file}: {e}")
+def label_encoder(df,colonne,labelencoder):
+    df[colonne]= labelencoder.fit_transform(df[colonne])
 
-    return data
+def clean_all_dataframe(fill_actions,fill_sectors,fill_bourses):
+    
+    fill_actions["Market"] = LabelEncoder().fit_transform(fill_actions["Market"])
+    
+    fill_actions["Sector"] = LabelEncoder().fit_transform(fill_actions["Sector"])
+    fill_actions['Ticker'] = LabelEncoder().fit_transform(fill_actions['Ticker'])
+    fill_sectors['indice'] = LabelEncoder().fit_transform(fill_sectors['indice'])
+    fill_bourses['indice'] = LabelEncoder().fit_transform(fill_bourses['indice'])
+    return fill_actions,fill_sectors,fill_bourses
+
+def fusion(liste):
+    df=pd.concat(liste, ignore_index=True)
+    return df
+
+def load_all():
+    sectors = load_indices(f"{BASE_PATH}/sector_indices")
+    bourses=load_indices(f"{BASE_PATH}/bourse_indices")
+    # actions
+    actions = load_actions(BASE_PATH)
+    print(f"\n✅ Loaded {len(actions)+len(sectors)+len(bourses)} files")
+    return actions,sectors,bourses
+
+def concat_dataframe():
+    
+    actions,sectors,bourses=load_all()
+    
+    # ── Fusion ──
+    fill_sectors= fusion(sectors)
+    fill_action = fusion(actions)
+    fill_bourse = fusion(bourses)
+    return fill_action,fill_sectors,fill_bourse
+
+def prepare_external_df(df, key_name, suffix):
+    
+    df = df.rename(columns={"indice": key_name})
+
+    cols_to_rename = {
+        col: f"{col}_{suffix}"
+        for col in df.columns
+        if col not in ["Date", key_name,"is_trading_day"]
+    }
+
+    df = df.rename(columns=cols_to_rename)
+
+    return df
+
+def merge_dataset(left_df, right_df, keys):
+    
+    return left_df.merge(
+        right_df,
+        on=keys,
+        how="left"
+    )
+    
+def merge_all_datasets(actions, sectors, bourses):
+
+    sectors = prepare_external_df(
+        sectors,
+        key_name="Sector",
+        suffix="sector"
+    )
+
+    bourses = prepare_external_df(
+        bourses,
+        key_name="Market",
+        suffix="bourse"
+    )
+
+    df = merge_dataset(
+        actions,
+        sectors.drop(columns=["is_trading_day"]),
+        ["Date", "Sector"]
+    )
+
+    df = merge_dataset(
+        df,
+        bourses.drop(columns=["is_trading_day"]),
+        ["Date", "Market"]
+    )
+
+    return df
 
 def prepare_datasets():
-
     indices = load_indices(os.path.join(BASE_PATH, "sector_indices"))
     bourses = load_indices(os.path.join(BASE_PATH, "bourse_indices"))
     actions = load_actions(BASE_PATH)
@@ -167,14 +221,36 @@ def prepare_datasets():
     fill_bourse = pd.concat(bourses, ignore_index=True)
     fill_df = pd.concat(actions, ignore_index=True)
 
-    fill_df["Ticker_name"] = fill_df["Ticker"].astype(str)
+    fill_df = pd.get_dummies(fill_df, columns=["Market", "Sector"])
 
     ticker_encoder = LabelEncoder()
-    sector_encoder = LabelEncoder()
-    market_encoder = LabelEncoder()
+    indice_encoder = LabelEncoder()
+    bourse_encoder = LabelEncoder()
 
-    fill_df["Ticker"] = ticker_encoder.fit_transform(fill_df["Ticker"].astype(str))
-    fill_df["Sector"] = sector_encoder.fit_transform(fill_df["Sector"].astype(str))
-    fill_df["Market"] = market_encoder.fit_transform(fill_df["Market"].astype(str))
+    fill_df["Ticker"] = ticker_encoder.fit_transform(fill_df["Ticker"])
+    fill_indice["indice"] = indice_encoder.fit_transform(fill_indice["indice"])
+    fill_bourse["indice"] = bourse_encoder.fit_transform(fill_bourse["indice"])
 
     return fill_indice, fill_df, fill_bourse, ticker_encoder
+
+def main():  
+    
+    fill_actions,fill_sectors,fill_bourses = concat_dataframe()
+    
+    fill_actions,fill_sectors,fill_bourses= clean_all_dataframe(fill_actions,fill_sectors,fill_bourses)
+    
+    dataframe_info(fill_actions)
+    dataframe_info(fill_bourses)
+    dataframe_info(fill_sectors)
+    
+    final_df = merge_all_datasets(
+    fill_actions,
+    fill_sectors,
+    fill_bourses
+    )
+    dataframe_info(final_df)
+    
+    return fill_actions,fill_sectors,fill_bourses,final_df
+
+if __name__ == "__main__":
+    fill_actions,fill_sectors,fill_bourses, final_df=main()
